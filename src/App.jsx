f17378bc415a11f82c0873293e06df046404f9ed
Voranat W. <voranat.wn@gmail.com>
2026-01-12 12:07:04 +0700

    chore: initial project setup with react, vite, and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+    <div className="min-h-screen bg-gradient-to-r from-purple-600 to-indigo-600 flex flex-col items-center justify-center p-8">
+      <h1 className="text-5xl font-bold text-white mb-8">Movie Survey</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
